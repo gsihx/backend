38c0829d13364de0395e7e37a70515ecfea1fcda
Kirill Burdin <burdin_109@mail.ru>
2026-04-08 01:47:17 +0700

    fix: global url update and route paths

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def handle_preflight():
 
 
 # ИСПРАВЛЕНО: Изменен метод на GET, чтобы не конфликтовать с добавлением
-@app.route('tasks', methods=['GET'])
+@app.route('/api/tasks', methods=['GET'])
 def get_tasks():
     subject = request.args.get('subject', 'Все')
     variant = request.args.get('variant', 'Все')
@@ -418,7 +418,7 @@ def get_achievements(current_user_id):
     return jsonify({'achievements': achievements})
 
 
-@app.route('user_solved_tasks', methods=['GET'])
+@app.route('/api/user_solved_tasks', methods=['GET'])
 @token_required
 def get_user_solved_tasks(current_user_id):
     try:
